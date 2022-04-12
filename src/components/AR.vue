68d4dab4f436dcf5f5195bbe5025e04498f12d9d
aleksandrtynanov <aleksandr.tynianov.ddi@gmail.com>
2022-04-12 20:36:13 +0300

    update ar component. add scene

--- src/components/AR.vue
+++ src/components/AR.vue
@@ -1,26 +1,14 @@
 <template>
-  <div>
-    <a-scene embedded arjs='sourceType: webcam; debugUIEnabled: false; detectionMode: mono_and_matrix; matrixCodeType: 3x3;'>
-
-      <a-assets>
-        <a-asset-item id="animated-asset" src="https://raw.githubusercontent.com/nicolocarpignoli/nicolocarpignoli.github.io/master/ar-playground/models/CesiumMan.gltf"></a-asset-item>
-      </a-assets>
-
-      <a-marker type='barcode' value='7'>
-        <a-box position='0 0.5 0' color="yellow"></a-box>
-      </a-marker>
-
-      <a-marker id="animated-marker" type='barcode' value='6'>
-        <a-entity
-            gltf-model="#animated-asset"
-            scale="2">
-        </a-entity>
-      </a-marker>
-
-      <!-- use this <a-entity camera> to support multiple-markers, otherwise use <a-marker-camera> instead of </a-marker> -->
-      <a-entity camera></a-entity>
-    </a-scene>
-  </div>
+  <a-scene embedded arjs>
+    <a-marker preset="hiro">
+      <a-entity
+          position="0 0 0"
+          scale="0.05 0.05 0.05"
+          gltf-model='../assets/scene.gltf'
+      ></a-entity>
+    </a-marker>
+    <a-entity camera></a-entity>
+  </a-scene>
 
 </template>
 
@@ -29,7 +17,12 @@
 import "@ar-js-org/ar.js";
 
 export default {
-  name: "AR"
+  name: "AR",
+  methods: {
+    markerFoundEvent(e) {
+        console.log(e)
+    }
+  }
 }
 </script>
 
